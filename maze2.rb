0348e6f31b084428925e06c871c9a1945c2f3900
yohei-koba-tokyo <60637308+yohei-koba-tokyo@users.noreply.github.com>
2020-05-25 10:55:06 +0900

    maze2

--- maze2.rb
+++ maze2.rb
@@ -1,19 +1,16 @@
 # 以下コードはテスト中です
 
 
-def cross
-  [imap[iam[0], iam[1]-1], imap[iam[0]-1, iam[1]], imap[iam[0], iam[1]+1], imap[iam[0]+1, iam[1]]]
-end
-
-imap = []
+table = []
 words = []
 
+# 入力ゾーン
 N = gets.to_i
-imap << ("."*(N+2))
+table << ("."*(N+2))
 N.times do
-  imap << "."+gets.chomp+"."
+  table << "."+gets.chomp+"."
 end
-imap << ("."*(N+2))
+table << ("."*(N+2))
 
 M = gets.to_i
 M.times do
@@ -21,64 +18,41 @@ M.times do
 end
 
 
-puts imap
-puts words
-
-
-
-
-
-
 
 words.each do |word|
-  answere = "no"
+
   start_point = []
-  imap.each_with_index do |line, index|
-    start = line.each_index.select{|i| line[i] == word[0]}
+  table.each_with_index do |line, index|
+    start = line.chars.each_index.select{|i| line[i] == word[0]}
     start.each do |s|
       start_point << [index, s]
     end
   end
-  end
-  break if start_point = []
 
-
-
-
-  word.drop(1)
-
-
-  start_point do |iam|
-    iimap = imap
-    iimap[iam[0], iam[1]] = "."
-    word.each do |w|
-      # cross = [iimap[iam[0], iam[1]-1], iimap[iam[0]-1, iam[1]], iimap[iam[0], iam[1]+1], iimap[iam[0]+1, iam[1]]]
-      # iam = cross.each_index.select{|i| cross[i] == w}
+  end_point = []
+  table.each_with_index do |line, index|
+    start = line.chars.each_index.select{|i| line[i] == word.chars.last}
+    start.each do |s|
+      end_point << [index, s]
     end
   end
 
+  answere = "no"
+  word.slice!(0)
+  # print "start "
+  # p start_point
+  # print "end   "
+  # p end_point
+  start_point.each_with_index do |sp, s|
+    end_point.each_with_index do |ep, e|
+      distance = (ep[0]-sp[0]).abs + (ep[1]-sp[1]).abs
+      if distance != 0 && distance <= word.length && (word.length - distance).even? && word.length <= N**2 - 1
+        # puts "      test OK"
 
-  puts answere
+      
+      end
+    end
+  end
+
 end
 
-
-
-words.each do |word|
-  iimap =imap
-  answere = "no"
-  
-  word.each do |w|
-    iam = []
-    imap.each_with_index do |line, index|
-      search = line.each_index.select{|i| line[i] == w}
-      search.each do |s|
-        iam << [index, s]
-      end
-      iam.each do |iam|
-        cross = [imap[iam[0], iam[1]-1], imap[iam[0]-1, iam[1]], imap[iam[0], iam[1]+1], imap[iam[0]+1, iam[1]]]
-        cross.each do |c|
-          imap[c[0]][c|1|] == 
-      end
-
-    end
-  end
