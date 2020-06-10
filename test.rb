cf10e67ebe72fe965c2e45edd34319ab73047884
yohei-koba-tokyo <60637308+yohei-koba-tokyo@users.noreply.github.com>
2020-06-10 09:56:59 +0900

    vuejs_practice

--- test.rb
+++ test.rb
@@ -1,7 +1,21 @@
-begin
-  # 数字を0で割ろうとすると、ZeroDivisionError例外が発生する
-    1 / 0
-  rescue
-  # 例外が発生した時にrescue以下の処理が呼ばれる
-  puts '0で割ることはできません'
+
+def lone_sum(*ary)
+  # 1. 配列から、重複する要素を取り出す
+  un_uniqs = []
+  ary.each_with_index do |n, i|
+    cp_ary = ary.drop(i+1)
+
+    cp_ary.each do |cn|
+      un_uniqs << n if n == cn
+    end
+
+  end
+  # 2. 重複した要素をuniqする
+  uniq_un_uniqs = un_uniqs.uniq
+  # 3. 重複する数字と元の配列の数字を比較し、もし重複する数字でなければ足し合わせる
+  ary.inject(0) {|n, item| uniq_un_uniqs.include?(item) ? n + 0 : n + item }
 end
+
+# p lone_sum(1, 2, 3)
+# p lone_sum(3, 2, 3)
+p lone_sum(3, 3, 3)
