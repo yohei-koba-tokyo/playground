5b38436e8f04179f36309193c7be740e297c2c8b
yohei-koba-tokyo <60637308+yohei-koba-tokyo@users.noreply.github.com>
2020-06-14 10:24:35 +0900

    algorithm_practice

--- test.rb
+++ test.rb
@@ -1,21 +1,63 @@
 
-def lone_sum(*ary)
-  # 1. 配列から、重複する要素を取り出す
-  un_uniqs = []
-  ary.each_with_index do |n, i|
-    cp_ary = ary.drop(i+1)
 
-    cp_ary.each do |cn|
-      un_uniqs << n if n == cn
+
+
+
+
+
+# 3 3 10
+# 60 2 2 4
+# 70 8 7 9 
+# 50 2 3 9
+
+
+# スコアのリストを渡すと、スキル習得できるかを判定するメソッド
+def passing?(results, border)
+  results.each do |result|
+    if result < border
+      return false
+      break
     end
-
   end
-  # 2. 重複した要素をuniqする
-  uniq_un_uniqs = un_uniqs.uniq
-  # 3. 重複する数字と元の配列の数字を比較し、もし重複する数字でなければ足し合わせる
-  ary.inject(0) {|n, item| uniq_un_uniqs.include?(item) ? n + 0 : n + item }
+  return true
 end
 
-# p lone_sum(1, 2, 3)
-# p lone_sum(3, 2, 3)
-p lone_sum(3, 3, 3)
+# 購入する本のリストを渡すと、点数の結果を返すメソッド
+def scoring(books, qNumber)
+  results = [] + [0] * qNumber
+  books.each do |book|
+    qNumber.times do |i|
+      results[i] += book[i+1]
+    end
+  end
+  return results
+end
+
+# 入力
+bookNumber, qNumber, border = gets.split.map(&:to_i)
+books = []
+bookNumber.times do
+  books << gets.split.map(&:to_i)
+end
+
+# スキル習得がそもそも不可能だった場合に判定終了させる
+if not passing?(scoring(books, qNumber), border)
+  puts -1
+  exit
+end
+
+# クリアできる購入本のパターンを探し出し、全ケースの購入金額のみをpassing_listに入れる。
+passing_list = []
+bookNumber.times do |i|
+  # combinationメソッドは、「何個の本を買うとしたら、どの本を選ぶか」？の、全パターンをあぶり出すのに最適。
+  buy_books_pattern = books.combination(i+1).to_a
+  buy_books_pattern.each do |buy_books|
+    if passing?(scoring(buy_books, qNumber), border)
+      # transposeは、二次元配列のとき便利。仮想の表と判断し、行列を入れ替える
+      passing_list << buy_books.transpose[0].sum
+    end
+  end
+end
+
+# 最小値を出力
+puts passing_list.min
---
+++ test2.rb
@@ -0,0 +1,45 @@
+
+
+
+
+
+
+# 3 600
+# 600 200 200 400
+# 900 800 400 500
+# 200 200 200 300
+
+
+
+# タクシー
+
+# 追加料金を返すメソッド
+def add_price(distance, taxi)
+  (((distance - taxi[0]) / taxi[2]) + 1) * taxi[3]
+end
+
+# 料金を調べるメソッド
+def price(distance, taxi)
+  # ここで<とするか<=とするかが、ポイント
+  if taxi[0] <= distance
+    return taxi[1] + add_price(distance, taxi)
+  else
+    return taxi[1]
+  end
+end
+
+# 入力
+taxi_num, distance = gets.split.map(&:to_i)
+taxies = []
+taxi_num.times do
+  taxies << gets.split.map(&:to_i)
+end
+
+# 料金の全パターンを配列に格納
+price_list = []
+taxies.each do |taxi|
+  price_list << price(distance, taxi)
+end
+
+p price_list
+puts "#{price_list.min} #{price_list.max}"
